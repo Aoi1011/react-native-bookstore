050d1c095d23f93d95c6410e78c6811d219f43af
Aoi <ht.so.ik0102@gmail.com>
2022-03-20 19:37:02 +0700

    create horizontail view

--- app/components/Detail.tsx
+++ app/components/Detail.tsx
@@ -7,6 +7,7 @@ import {
   ImageBackground,
   ScrollView,
   TouchableHighlight,
+  StyleSheet,
 } from 'react-native';
 import RenderHTML from 'react-native-render-html';
 
@@ -42,33 +43,39 @@ const Detail = ({route, navigation}) => {
         <ActivityIndicator />
       ) : (
         <View>
-          <View
+          <ScrollView
             style={{
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor: '#AFAFAF',
+              // paddingTop: 10,
+              paddingLeft: 10,
+              paddingRight: 10,
             }}>
-            <MaterialIcon
-              name="book-open-page-variant-outline"
-              size="extraLarge"
-              color="black"
-            />
-          </View>
-          {/* <ImageBackground
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: '#AFAFAF',
+              }}>
+              <MaterialIcon
+                name="book-open-page-variant-outline"
+                size="extraLarge"
+                color="black"
+              />
+            </View>
+            {/* <ImageBackground
             // style={styles.item}
             resizeMode="cover"
             source={{uri: data.volumeInfo.imageLinks.thumbnail}}
             // width={deviceWidth / 2 - 4 * 2}
             // height={200}
           /> */}
-          <ScrollView
-            style={{paddingTop: 10, paddingLeft: 10, paddingRight: 10}}>
+
             <View
               style={{
                 flexDirection: 'row',
                 justifyContent: 'space-between',
                 alignContent: 'center',
+                paddingBottom: 30,
               }}>
               <Text style={{fontSize: 30, width: 330}}>
                 {data.volumeInfo.title}
@@ -85,17 +92,95 @@ const Detail = ({route, navigation}) => {
                 />
               </TouchableHighlight>
             </View>
-
-            <RenderHTML
-              source={{
-                html: data.volumeInfo.description,
+            <View
+              style={{
+                borderBottomColor: 'black',
+                borderBottomWidth: 1,
               }}
             />
+            <View style={{paddingTop: 30, paddingBottom: 30}}>
+              <Text
+                style={{
+                  color: 'grey',
+                  fontSize: 20,
+                  paddingBottom: 20,
+                  fontFamily: 'Cochin',
+                }}>
+                Publisher Description
+              </Text>
+              <RenderHTML
+                source={{
+                  html: data.volumeInfo.description,
+                }}
+              />
+            </View>
+            <View
+              style={{
+                borderBottomColor: 'black',
+                borderBottomWidth: 1,
+              }}
+            />
+            <ScrollView horizontal={true} style={{paddingTop: 30}}>
+              <View style={styles.horizontal}>
+                <Text style={styles.horizontalTitle}>GENRE</Text>
+                <Text style={styles.horizontalTitle}>
+                  {data.volumeInfo.categories[0]}
+                </Text>
+              </View>
+              <View
+                style={{
+                  borderEndColor: 'black',
+                  borderEndWidth: 1,
+                }}
+              />
+              <View style={styles.horizontal}>
+                <Text style={styles.horizontalTitle}>PUBLISHED DATE</Text>
+                <Text style={styles.horizontalTitle}>
+                  {data.volumeInfo.publishedDate}
+                </Text>
+              </View>
+              <View
+                style={{
+                  borderEndColor: 'black',
+                  borderEndWidth: 1,
+                }}
+              />
+              <View style={styles.horizontal}>
+                <Text style={styles.horizontalTitle}>PUBLISHER</Text>
+                <Text style={styles.horizontalTitle}>
+                  {data.volumeInfo.publisher}
+                </Text>
+              </View>
+              <View
+                style={{
+                  borderEndColor: 'black',
+                  borderEndWidth: 1,
+                }}
+              />
+              <View style={styles.horizontal}>
+                <Text style={styles.horizontalTitle}>LANGUAGE</Text>
+                <Text style={styles.horizontalTitle}>
+                  {data.volumeInfo.language.toUpperCase()}
+                </Text>
+              </View>
+            </ScrollView>
+            <Text style={{paddingBottom: 50}}>Hello</Text>
           </ScrollView>
         </View>
       )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  horizontal: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    height: 50,
+  },
+  horizontalTitle: {
+    textAlign: 'center',
+  },
+});
 
 export default Detail;
